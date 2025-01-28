f2a7e2b3c79a35a600cf2b9998b8e64ba86012a1
Ademola Madelewi <ademolamadelewi@gmail.com>
2025-01-28 03:31:44 +0100

    fix: remove unused dependencies

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,46 +1,19 @@
 // @ts-nocheck
-import { useEffect, useState } from "react";
 import CustomButton from "../Button";
-import {
-  HeaderContainer,
-  LogoContainer,
-  HeaderAddons,
-  HeaderDropdown,
-} from "./Header.styles";
-import { PeraWalletConnect } from "@perawallet/connect";
-import { setCurrentUser, setWalletType } from "../../stores/user/user.reducer";
-import { useDispatch, useSelector } from "react-redux";
-import { DeflyWalletConnect } from "@blockshake/defly-connect";
-import { RootState } from "../../stores/stores";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import { HeaderContainer, LogoContainer, HeaderAddons } from "./Header.styles";
 import { useNavigate, useLocation } from "react-router-dom";
-import { errorToast, infoToast, successToast } from "../../utils/customToast";
 import {
   useAppKit,
   useAppKitAccount,
-  useDisconnect,
   useWalletInfo,
 } from "@reown/appkit/react";
 
-const peraWallet = new PeraWalletConnect();
-const deflywallet = new DeflyWalletConnect();
-
 export const Header = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { open, close } = useAppKit();
-  const { address, isConnected, caipAddress, status, embeddedWalletInfo } =
-    useAppKitAccount();
+  const { open } = useAppKit();
+  const { address, isConnected } = useAppKitAccount();
   const { walletInfo } = useWalletInfo();
-
-  const { disconnect } = useDisconnect();
-
-  const [handleCopyAddress, setHandleCopyAddress] = useState(false);
-
-  const walletType = useSelector(
-    (state: RootState) => state.currentUser?.walletType
-  );
   const location = useLocation();
 
   return (
